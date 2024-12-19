b02b008a1f0fe8a4acdcced4e355d84810a7243f
Nolnzz <54479417+babisemata@users.noreply.github.com>
2024-12-20 02:53:16 +0800

    Update index.php

--- cobaweb/index.php
+++ cobaweb/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Selamat datang di website saya!</h1>";
+echo "<h1>Selamat datang di website nolanno!</h1>";
 echo "<p>Ini adalah halaman utama yang dibuat menggunakan PHP.</p>";
 echo "<p>Waktu sekarang: " . date("Y-m-d H:i:s") . "</p>";
 ?>
